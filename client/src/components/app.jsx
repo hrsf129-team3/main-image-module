2e2f78cd3ef6571dcb06b861e43c5a75f1af1547
maureenmlee <maureen.ml.lee@gmail.com>
2020-08-10 13:03:16 -0700

    Confirmed webpack update

---
+++ client/src/components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Components {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+
+  render() {
+    return (
+      <div>
+        Page is loading
+      </div>
+
+    );
+  }
+}
+
+export default App;
