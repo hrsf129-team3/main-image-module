168a69fb68ef8fae2798da2d700743e7608bc4b3
maureenmlee <maureen.ml.lee@gmail.com>
2020-08-14 14:53:33 -0700

    Updated unit testing

--- test/image.test.js
+++ test/image.test.js
@@ -1,13 +1,6 @@
 import React from 'react';
-// import Enzyme, { shallow } from 'enzyme';
 import Image from '../client/src/components/image.jsx';
-// import Image from '../components/image.jsx';
 
-//
-// const Adapter = require('enzyme-adapter-react-16');
-
-
-// Enzyme.configure({ adapter: new Adapter() })
 
 describe('Image', () => {
   it('should render image component', () => {
--- test/imageList.test.js
+++ test/imageList.test.js
@@ -8,23 +8,3 @@ describe('ImageList', () => {
   });
 })
 
-
-// describe('List tests', () => {
-
-//   it('renders list-items', () => {
-//     const items = ['one', 'two', 'three'];
-//     const wrapper = shallow(<ImageList allImages={[]} />);
-
-//     // Expect the wrapper object to be defined
-//     expect(wrapper.find('.list-items')).toBeDefined();
-//     expect(wrapper.find('.item')).toHaveLength(items.length);
-//   });
-
-//   it('renders correct text in item', () => {
-//     const items = ['John', 'James', 'Luke'];
-//     const wrapper = shallow(<List items={items} />);
-
-//     //Expect the child of the first item to be an array
-//     expect(wrapper.find('.item').get(0).props.children).toEqual('John');
-//   });
-// });
