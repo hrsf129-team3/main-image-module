781cb452e7d73b6bbdaceb8d6362464845859963
maureenmlee <maureen.ml.lee@gmail.com>
2020-08-19 02:06:19 -0700

    Updated flexbox on carousel

--- client/src/components/carousel.jsx
+++ client/src/components/carousel.jsx
@@ -87,11 +87,12 @@ class Carousel extends React.Component {
           </div>
           {this.showMainImage() }
           <div className={styles.iconsContainer}>
-            <div className={styles.heart}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 21C10.349 21 2 14.688 2 9c0-3.421 2.364-6 5.5-6A6.912 6.912 0 0112 5.051 6.953 6.953 0 0116.5 3C19.636 3 22 5.579 22 9c0 5.688-8.349 12-10 12zM7.5 5C5.472 5 4 6.683 4 9c0 4.108 6.432 9.325 8 10 1.564-.657 8-5.832 8-10 0-2.317-1.472-4-3.5-4-1.979 0-3.7 2.105-3.721 2.127l-.788.973-.775-.98C11.186 7.083 9.5 5 7.5 5z" /></svg>
+
+            <div className={styles.heartContainer}>
+              <svg className={styles.heart} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 21C10.349 21 2 14.688 2 9c0-3.421 2.364-6 5.5-6A6.912 6.912 0 0112 5.051 6.953 6.953 0 0116.5 3C19.636 3 22 5.579 22 9c0 5.688-8.349 12-10 12zM7.5 5C5.472 5 4 6.683 4 9c0 4.108 6.432 9.325 8 10 1.564-.657 8-5.832 8-10 0-2.317-1.472-4-3.5-4-1.979 0-3.7 2.105-3.721 2.127l-.788.973-.775-.98C11.186 7.083 9.5 5 7.5 5z" /></svg>
             </div>
-            <div className={styles.rightArrow} >
-              <svg onClick={this.nextImage} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M8 21a1 1 0 01-.664-1.747L15.5 12 7.336 4.747a1 1 0 011.328-1.494L18.5 12l-9.836 8.747A.994.994 0 018 21z" /></svg>
+            <div className={styles.rightArrowContainer} >
+              <svg className={styles.rightArrow} onClick={this.nextImage} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M8 21a1 1 0 01-.664-1.747L15.5 12 7.336 4.747a1 1 0 011.328-1.494L18.5 12l-9.836 8.747A.994.994 0 018 21z" /></svg>
             </div>
           </div>
         </div>
